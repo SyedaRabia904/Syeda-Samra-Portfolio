58eff66b5e5bb3738de61bde1cb4bc397762d7b8
uchakip <syedasamra812@gmail.com>
2023-08-04 19:24:05 +0500

    Second commit

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Contact from './contact';
+import Projects from './projects';
+import AboutMe from './aboutme';
+import App from './App';
+import Home from './home';
+import NotFound from './notfound';
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'aboutme',
+        element: <AboutMe/>,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'projects',
+        element: <Projects />,
+
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>,
+)
